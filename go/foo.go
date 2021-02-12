2177f4cdb9fda236c3074f0f1a2e7faf279634a1
stinarl <69670955+stinarl@users.noreply.github.com>
2021-02-12 12:24:18 +0100

    Update foo.go

--- go/foo.go
+++ go/foo.go
@@ -1,5 +1,3 @@
-// Use `go run foo.go` to run your program
-
 package main
 
 import (
@@ -12,16 +10,21 @@ var i = 0
 
 func incrementing() {
     //TODO: increment i 1000000 times
+	for j := 0; j < 1000000; j++ {
+		i++
+	}
 }
 
 func decrementing() {
     //TODO: decrement i 1000000 times
+	for k := 0; k < 1000000; k++ {
+		i--
+	}
 }
 
-func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
-	                                    // Try doing the exercise both with and without it!
 
+func main() {
+    runtime.GOMAXPROCS(runtime.NumCPU())    
     // TODO: Spawn both functions as goroutines
 	
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
